e798feb2e5d59f4b27b55c1b2a7e98fd2b06840c
Samy Bencherif <samybencherif73@gmail.com>
2026-03-14 11:05:50 -0400

    some friendly examples

--- examples/001-hello-world.js
+++ examples/001-hello-world.js
@@ -1,6 +1,71 @@
+// ▗▖  ▗▖ ▗▄▄▖▗▄▄▄  ▗▄▄▖  ▗▄▖ ▗▖ ▗▖
+// ▐▛▚▖▐▌▐▌   ▐▌  █ ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌
+// ▐▌ ▝▜▌ ▝▀▚▖▐▌  █ ▐▛▀▚▖▐▛▀▜▌▐▌ ▐▌
+// ▐▌  ▐▌▗▄▄▞▘▐▙▄▄▀ ▐▌ ▐▌▐▌ ▐▌▐▙█▟▌
+//
+// Hello and welcome to NSDraw ❤️. 
+// This website is about making interactive graphical programs.
+// The goal is to share the joy of programming as an act of creative expression.
 
-// clear the screen to the color white
-clear({ color: "white" });
+// These lines, which start with "//", are comments. They are ignored by the computer,
+// and can be used to write helpful hints for human readers.
 
-// draw a black circle in the middle of the screen
-draw_circle({x:W/2, y:H/2, r:100, fill:"black"});
+// Let's get started with our first program!
+
+// We can change the colors on the screen using functions.
+// Functions are how we tell the computer what to do.
+
+// Here we are calling the "clear" function, which makes the whole screen one color.
+
+// Try changing the color from "magenta" to "white".
+// Press the [ ▶ Run ] button in the top left corner to see the program in action.
+clear({ color: "magenta" });
+
+// Lets examine the syntax of this line of code. 
+// Syntax refers to the structure of letters and symbols that make up the code.
+
+// I will use arrows (^^^) to point to different parts of the line. 
+
+// We start with the name of the function.
+// clear({ color: "magenta"});
+// ^^^^^
+
+// Then we have parentheses which tell us we would like to execute the function.
+// They also provide a space to specify information about how we want it to be done.
+// clear({ color: "magenta"});
+//      ^                   ^
+
+// Next the curly brackets specify an object. An object contains information that can
+// be read by the function definition. I'll show you how to do this later.
+// clear({ color: "magenta"});
+//       ^                 ^
+
+// The object contains keys and values. Keys give names to data and values specify the data itself.
+// The "clear" function expects an object that defines the "color" key with a value of a specific color.
+
+// So provide the key "color" in our object.
+// clear({ color: "magenta"});
+//         ^^^^^
+
+// Then we use the colon to separate the key from the value.
+// clear({ color: "magenta"});
+//              ^
+
+// The value is a string, which is enclosed in quotes.
+// Here we can say the name of a color.
+// clear({ color: "magenta"});
+//                ^^^^^^^^^
+
+// Finally we end the line with a semicolon.
+// clear({ color: "magenta"});
+//                           ^
+
+// Phew! That was a lot of detail for a single line of code,
+// but you made it this far, and now you understand the fundamental
+// language we use to communicate with the computer.
+
+// Continue to the next example 002-colors.js, to see what else you can do
+// with the language of function calls.
+
+// By the way, we are using Javascript coding language, but this
+// principle applies to many other languages as well.
---
+++ examples/003-squares.js
@@ -0,0 +1,30 @@
+
+// First let's just soak in the program.
+
+clear({ color: "gray" });
+
+draw_rect({ x: 10, y: 10, w: 100, h: 100, fill: "red" });
+draw_rect({ x: 120, y: 10, w: 100, h: 100, fill: "yellow" });
+draw_rect({ x: 230, y: 10, w: 100, h: 100, fill: "blue" });
+
+// Feel free to change some of the parameters above and see how the output changes.
+// If you get any errors, click on 003-squares.js in the left panel to reset the code.
+
+// Now let's explain what needs to be explained.
+
+// We are calling a new function! draw_rect draws rectangles.
+// draw_rect({ x: 10, y: 10, w: 100, h: 100, fill: "red" });
+// like before, we're starting with the function name,
+// passing in an object, and defining keys and values.
+
+// 1. Try moving the red square to the right, by changing its x value to 400. (hint, it's on line 6)
+// 2. Now move it down by changing the y value to 300.
+// 3. Change the color to whatever you want. (For example "#258042")
+
+// 4. (optional) Feel free to get creative. Add or remove squares by copying lines of code.
+//    Change the colors and positions to create a programmatic illustration.
+
+// If you like what you've made, you can copy the code, create a new file (call it, for example, first.js)
+// and paste the code in the new file. It will be saved in your browser's localStorage.
+
+// You can also save the file to your desktop or share it with others.
